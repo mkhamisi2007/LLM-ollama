5a5ab05663c7ad05684af01764c6e2e1d4fdfc26
mkhamisi2007 <31432794+mkhamisi2007@users.noreply.github.com>
2026-05-10 12:11:56 +0200

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAIOSFODNN7EXAMPLE"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+}
+
+resource "aws_s3_bucket" "data" {
+  bucket = "my-company-data"
+  acl    = "public-read"
+}
+
+resource "aws_security_group" "allow_all" {
+  name = "allow_all"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
